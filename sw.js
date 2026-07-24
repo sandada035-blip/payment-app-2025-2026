861e3f7416133b8a87b05067fc9c8a6b8a26b0e8
sandada035-blip <sandada035@gmail.com>
2026-07-24 17:25:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'akk-mohesey-v3';
+const CACHE_NAME = 'akk-mohesey-v10';
 const urlsToCache = [ '/', '/index.html', '/manifest.json' ];
 
 self.addEventListener('install', event => {
